da17d07cc8f6a0d51b330fc6191bc874b7e487e1
Matias Spina Mattera <rubiioibz@hotmail.com>
2022-05-06 03:59:36 +0200

    cambio tamaño slider

--- src/components/Carrusel.js
+++ src/components/Carrusel.js
@@ -3,24 +3,21 @@ import { UncontrolledCarousel, Container } from "reactstrap";
 
 const Carrusel = () => {
   return (
-    <Container fluid>
+    <Container fluid className="w-50">
       <UncontrolledCarousel
         items={[
           {
             altText: "Aceites",
-            caption: "Aceites",
             key: 1,
             src: "https://img.freepik.com/foto-gratis/trituradora-metal-aceite-cannabis-articulacion-posar_1150-20693.jpg?t=st=1651607073~exp=1651607673~hmac=fe7955fbbaa2dcd7a4cce7b925b23da58daa79afc594ed65b2d372a39f65d07f&w=1380",
           },
           {
             altText: "Accesorios",
-            caption: "Accesorios",
             key: 2,
             src: "https://img.freepik.com/foto-gratis/disposicion-gotero-aceite-cbd_23-2149013546.jpg?t=st=1651607073~exp=1651607673~hmac=a794ce33bc477394ddbfaf85c7bd11824c6f38cfa876533e0dc3ec646175a93b&w=1380",
           },
           {
             altText: "Semillas",
-            caption: "Semillas",
             key: 3,
             src: "https://img.freepik.com/foto-gratis/primer-plano-hoja-marihuana-cannabis_1150-15077.jpg?t=st=1651607073~exp=1651607673~hmac=3a1b64446a9a61b7a81316655227faa53b700054d85f06da6cd4b3d9c5b8eea8&w=1380",
           },
